8ffb4f4ce0e894ead5d25cec107fb06d6d0e35db
Alex <anassif2@calstatela.edu>
2017-11-25 23:24:19 -0800

    lesson 2

--- app/src/main/java/com/alexnassif/mobile/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/alexnassif/mobile/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.alexnassif.mobile.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.alexnassif.mobile.coderswag.Model.Category
 import com.alexnassif.mobile.coderswag.R
+import com.alexnassif.mobile.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var adapter: ArrayAdapter<Category>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/alexnassif/mobile/coderswag/Model/Category.kt
+++ app/src/main/java/com/alexnassif/mobile/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.alexnassif.mobile.coderswag.Model
 /**
  * Created by raymond on 11/25/17.
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
